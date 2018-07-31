118ca0c5e7e5c9e65a650443574f1b3895c4f657
glacarn <glacarn@gmail.com>
2018-07-31 22:32:33 +0200

    Initialisation du projet MagiWorld

---
+++ README.md
@@ -0,0 +1,15 @@
+# MagiWorld
+
+--------------------------
+Jeux MagiWorld en Java
+--------------------------
+
+Activité OpenClassRoom :
+Apprenez l'objet avec Java
+
+--------------------------
+
+Jeux en version Terminal pour 2 joueurs
+
+3 classes :
+Guerrier, Rodeur, Mage
---
+++ src/magiworld/MagiWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magiworld;
+
+/**
+ *
+ * @author Arno
+ */
+public class MagiWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
